f70233c24e048564d5ea3c582061f90aeb8a7192
comp500 <comp500@users.noreply.github.com>
2015-04-10 07:13:22 +0100

    add lib WIP

---
+++ lib.lua
@@ -0,0 +1,13 @@
+-- lib() for CC by comp500
+-- MIT license - https://github.com/comp500/CCSnippets/
+function lib(...)
+    local c = fs.combine
+    local function fn(a) return fs.exists(a) and a or false end
+    for i,v in ipairs(arg) do
+        local file = fn(c('usr/apis',v)) or fn(v) or fn(shell.resolveProgram(v))
+        if not file then
+            local pm = fn('usr/bin/packman') or fn('usr/bin/ac-get')
+            shell.run(pm, "")
+        end
+    end
+end
