209509b9d9c3c003d845afcdd6dd13bf37a5d5c1
comp500 <comp500@users.noreply.github.com>
2015-04-10 07:31:46 +0100

    table

--- lib.lua
+++ lib.lua
@@ -1,13 +1,16 @@
 -- lib() for CC by comp500
 -- MIT license - https://github.com/comp500/CCSnippets/
 function lib(...)
-    local c = fs.combine
+    local c,r = fs.combine,{}
     local function fn(a) return fs.exists(a) and a or false end
     for i,v in ipairs(arg) do
         local file = fn(c('usr/apis',v)) or fn(v) or fn(shell.resolveProgram(v))
         if not file then
             local pm = fn('usr/bin/packman') or fn('usr/bin/ac-get')
             shell.run(pm, "")
+        else
+            table.insert(a,file)
         end
     end
+    return r
 end
