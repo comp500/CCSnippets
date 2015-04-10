08cdf3f0a97106e6007f8852826ce3e5996a98fd
comp500 <comp500@users.noreply.github.com>
2015-04-10 07:44:11 +0100

    add awesomesauce

--- lib.lua
+++ lib.lua
@@ -1,16 +1,20 @@
 -- lib() for CC by comp500
 -- MIT license - https://github.com/comp500/CCSnippets/
 function lib(...)
-    local c,r = fs.combine,{}
+    local c,r,s = fs.combine,{},shell.resolveProgram
     local function fn(a) return fs.exists(a) and a or false end
     for i,v in ipairs(arg) do
-        local file = fn(c('usr/apis',v)) or fn(v) or fn(shell.resolveProgram(v))
+        local file = fn(c('usr/apis',v)) or fn(v) or s(v)
         if not file then
-            local pm = fn('usr/bin/packman') or fn('usr/bin/ac-get')
-            shell.run(pm, "")
+            local pm = s('packman') or s('ac-get')
+            if http and pm then
+            else
+                printError("API "..v.." not found")
+                table.insert(r,nil)
+            end
         else
-            table.insert(a,file)
+            table.insert(r,file)
         end
     end
-    return r
+    return unpack(r)
 end
